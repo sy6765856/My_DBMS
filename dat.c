a37f2d80a8648c40b0b12f03f00db7328a501470
Your Name <you@example.com>
2013-06-28 15:29:23 +0800

    fdfsfs

--- dat.c
+++ dat.c
@@ -3,21 +3,67 @@
 #include <string.h>
 #include <stdlib.h>
 #include "dat.h"
+#include "dbf.h"
 #include "types.h"
+#include <sys/mman.h> 
+struct TB
+{
+    int pre_col;
+    int pre_row;
+    int nxt_col;
+    int nxt_row;
+    int tail;
+};
 int i;
+int error(char err[])
+{
+    printf("\e[1;31mERROR:");
+    puts(err);
+    printf("\e[1;32m");
+    return 0;
+}
+void init()
+{
+    char *memory = NULL;  
+    int file_length = 0;  
+    char *start_address = 0;  
+    int line_num = 0;  
+    int time_start = time(NULL);  
+    int fd = open( argv[1], O_RDONLY );
+    if ( fd > 0 )  
+    {  
+        file_length = lseek(fd, 1, SEEK_END);  
+        memory = mmap( start_address, file_length, PROT_READ, MAP_SHARED, fd, 0 );  
+        close( fd );  
+        munmap( memory, file_length );
+    }
+}
 int insert(char tb_name[],char in_f[LEN][M],int cpf,char in_v[LEN][M],int cpv)
 {
+    if(cpf&&cpf!=cpv)return error("values not match");
+    /* get the absolute position in dat file */
+    TableNode nd;
+    strcpy(nd.table.table_name,tb_name);
+    int tb_pos=get(nd);
+    if(cpf)
+    {
+        
+    }
+    else
+    {
+        
+    }
     printf ("%s\n",tb_name);
     printf ("%d %d\n",cpf,cpv);
     for(i=0;i<cpv;i++)puts(in_f[i]);
     for(i=0;i<cpv;i++)puts(in_v[i]);
-    return 0;
+    return 1;
 }
 
 int delete(char tb_name[])
 {
     printf ("%s\n",tb_name);
-    return 0;
+    return 1;
 }
 
 int update(char tb_name[],char col_name[],char cond[LEN][M],int cpf)
@@ -26,7 +72,7 @@ int update(char tb_name[],char col_name[],char cond[LEN][M],int cpf)
     printf ("col:%s\n",col_name);
     printf("%d\n",cpf);
     for(i=0;i<cpf;i++)puts(cond[i]);
-    return 0;
+    return 1;
 }
 
 
--- dbf.h
+++ dbf.h
@@ -17,34 +17,47 @@ typedef struct {
     Boolean key;
     Boolean null_flag;
     Boolean valid_flag;
-} TableMode, *p_TableMode;
+} Column, *p_Column;
+
+typedef struct {
+    Column table_mode;
+    int next_column;
+    int pre_column;
+} ColumnNode, *p_ColumnNode;
 
 typedef struct {
     char table_name[TABLE_NAME_LENGTH];
-    int num_table_mode;
-    int offset;
+    int num_column;
 } Table, *p_Table;
+
+typedef struct {
+    Table table;
+    int pre_table;
+    int next_table;
+    int head_column;
+    int tail_column;
+} TableNode, *p_TableNode;
 
 int create_database(char db_name[]);
 int drop_database(char db_name[]);
 int use_database(char db_name[]);
 
 int show_tables();
-int create_table(Table, p_TableMode);
-int alter_table(char table_name[], TableMode, AlterMode);
+int create_table(TableNode, p_ColumnNode);
+int alter_table(char table_name[], Column, AlterMode);
 int drop_table(char table_name[]);
 int show_table(char table_name[]);
 int rename_table(char table_name[], char new_name[]);
 
-int add_column(Table table, TableMode);
-int drop_column(Table table, TableMode);
-int alter_column(Table table, TableMode);
+int add_column(TableNode table_node, Column);
+int drop_column(TableNode table_node, Column);
+int alter_column(TableNode table_node, Column);
 
 Boolean check_exist_db(char db_name[]);
-Boolean check_exist_table(char *, FILE*);
-int gen_tables(Table tables[], FILE *fp);
-int get_table(char table_name[], p_Table table);
-int get_modes(Table table, p_TableMode table_modes);
+Boolean check_exist_table(char table_name[]);
+int gen_tables(FILE *fp);
+int get_table_node(char table_name[], p_TableNode table_node);
+int get_modes(TableNode table_node, p_Column columns);
 char *add_dbf_ext(char db_name[], char db_file_name[]);
 
 #endif
